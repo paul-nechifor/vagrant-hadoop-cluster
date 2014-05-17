f81692d1755802e058066347b67c4ec8e739d96a
Paul Nechifor <paul@nechifor.net>
2014-05-17 18:26:15 +0300

    Add more Hadoop environment vars.

--- scripts/hadoop-config-user.sh
+++ scripts/hadoop-config-user.sh
@@ -1,3 +1,5 @@
 # Add Hadoop commands to the path for easier SSH-ing.
 echo "export HADOOP_PREFIX=/opt/hadoop" >> ~/.bashrc
+echo "export HADOOP_HOME=/opt/hadoop" >> ~/.bashrc
+echo "export HADOOP_VERSION=1.2.1" >> ~/.bashrc
 echo "export PATH=\$PATH:/opt/hadoop/bin" >> ~/.bashrc
