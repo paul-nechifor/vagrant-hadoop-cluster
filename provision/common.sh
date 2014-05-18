1c9e4028eeedf7b698916da3932ca0a4850f77fd
Paul Nechifor <paul@nechifor.net>
2014-05-18 12:34:22 +0300

    Replace hosts file so slaves can communicate.

--- provision/common.sh
+++ provision/common.sh
@@ -5,6 +5,7 @@ bash $s/packages-install.sh
 bash $s/hadoop-install.sh
 bash $s/hadoop-config.sh
 bash $s/hadoop-config-user.sh
+bash $s/replace-hosts.sh
 
 su $username -c "bash $s/hadoop-config-user.sh"
 su $username -c "bash $s/ssh-authorize-master.sh"
---
+++ scripts/replace-hosts.sh
@@ -0,0 +1,1 @@
+cat /vagrant/files/etc/hosts > /etc/hosts
