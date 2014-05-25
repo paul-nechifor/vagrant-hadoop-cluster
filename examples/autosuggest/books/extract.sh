dc971419a1a56c522d12fde58fce3ac96d3d112a
Paul Nechifor <paul@nechifor.net>
2014-05-25 13:05:46 +0300

    Add download scripts.

---
+++ examples/autosuggest/books/extract.sh
@@ -0,0 +1,12 @@
+sudo apt-get install -y unzip # If you don't have it.
+
+mkdir txt
+find . -name '*.zip' -size -500k -exec unzip {} -d txt \;
+
+hadoop fs -rmr /txt
+hadoop fs -rmr /ngrams
+hadoop fs -copyFromLocal txt /txt
+hadoop jar ../target/autosuggest*.jar net.nechifor.autosuggest.App /txt /ngrams
+hadoop fs -cat /ngrams/part-00000
+
+rm -fr txt
